f8e6d6f9dc1c64334019e6fd344569e6f25fb78c
Gabe Berke-Williams <gabebw@gabebw.com>
2015-09-26 18:28:58 -0700

    Decode some JSON

--- Gorls.go
+++ Gorls.go
@@ -3,11 +3,9 @@ package main
 // http://rhymebrain.com/talk?function=getRhymes&word=coin&maxResults=0&lang=en
 
 import (
-	// "bufio"
+	"encoding/json"
 	"fmt"
-	// "io"
 	"io/ioutil"
-	// "os"
 )
 
 // Gross
@@ -15,6 +13,41 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+type RhymebrainResult struct {
+	Word      string `json:"word"`
+	Freq      int    `json:"freq"`
+	Score     int    `json:"score"`
+	Flags     string `json:"flags"`
+	Syllables string `json:"syllables"`
+}
+
+// Rhymes are in rhyme_service.rb
+
+func rhymes(word []byte) {
+	many := []byte(`
+		[ {"word":"goin","freq":19,"score":300,"flags":"c","syllables":"1"},
+		{"word":"join","freq":23,"score":300,"flags":"bc","syllables":"1"},
+		{"word":"groin","freq":18,"score":300,"flags":"bc","syllables":"1"},
+		{"word":"loin","freq":18,"score":300,"flags":"bc","syllables":"1"},
+		{"word":"doyenne","freq":13,"score":300,"flags":"b","syllables":"1"},
+		{"word":"groyne","freq":13,"score":300,"flags":"b","syllables":"1"},
+		{"word":"quoin","freq":13,"score":300,"flags":"b","syllables":"1"},
+		{"word":"adjoin","freq":16,"score":300,"flags":"bc","syllables":"2"},
+		{"word":"enjoin","freq":18,"score":300,"flags":"bc","syllables":"2"},
+		{"word":"rejoin","freq":18,"score":300,"flags":"bc","syllables":"2"},
+		{"word":"conjoin","freq":15,"score":300,"flags":"bc","syllables":"2"},
+		{"word":"purloin","freq":14,"score":300,"flags":"bc","syllables":"2"},
+		{"word":"subjoin","freq":16,"score":300,"flags":"b","syllables":"2"},
+		{"word":"sirloin","freq":15,"score":300,"flags":"b","syllables":"2"},
+		{"word":"tenderloin","freq":15,"score":300,"flags":"bc","syllables":"3"}]
+	`)
+	results := make([]RhymebrainResult, 0)
+	err := json.Unmarshal(many, &results)
+
+	check(err)
+	fmt.Println(results)
 }
 
 func main() {
@@ -32,5 +65,6 @@ func main() {
 		check(err)
 		phraseLines = append(phraseLines, string(dat))
 	}
-	fmt.Println(phraseLines)
+	rhymes([]byte("coin"))
+	// fmt.Println(phraseLines)
 }
